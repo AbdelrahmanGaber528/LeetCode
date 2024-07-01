f4b6b77847412ceeadc10d4ecee871c9cc137240
AbdelrahmanGaber528 <144827977+AbdelrahmanGaber528@users.noreply.github.com>
2024-07-02 01:46:18 +0300

    inhance the code

--- DailyChanllenge/threeConsecutiveOdds.java
+++ DailyChanllenge/threeConsecutiveOdds.java
@@ -2,18 +2,12 @@ class Solution {
     public boolean threeConsecutiveOdds(int[] arr) {
         int sumOdds =0;
         for(int i =0; i < arr.length ; i++){
-            if(arr[i]%2==1){
-                for(int j = i;j<arr.length;j++){
-                    if(arr[j]%2==1){
-                        sumOdds++;
-                    }
-                    else break;
-                    if(sumOdds==3){
-                        return true;
-                    }
-                }
-                sumOdds=0;
-            }
+            if(arr[i]%2==1)
+                sumOdds++;
+            else sumOdds=0;
+            if(sumOdds==3)
+                return true;
+
         }
         return false;
     }
